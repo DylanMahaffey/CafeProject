92fb1ee23cfb7984ece785438b466f55ac742bf5
Dylan Mahaffey <dylan.mahaffey@gmail.com>
2020-12-16 13:15:11 -0800

    model fixes

--- CafeApp/src/main/java/com/revature/models/Employee.java
+++ CafeApp/src/main/java/com/revature/models/Employee.java
@@ -3,5 +3,9 @@ package com.revature.models;
 import java.util.List;
 
 public class Employee extends User {
+	
+	public Employee(String name, String email, String password) {
+		super(name, email, password);
+	}
 
 }
