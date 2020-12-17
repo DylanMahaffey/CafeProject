50b70e2e441634db6cd205ff26d67c40b8673aac
freemwil <willparkerf@gmail.com>
2020-12-17 15:35:13 -0500

    a

--- CafeApp/src/main/java/com/revature/Router.java
+++ CafeApp/src/main/java/com/revature/Router.java
@@ -37,9 +37,9 @@ public class Router {
 	
 	public static void loggingInUser(User u) {
 		StateManager.loggedInUser = u;
-		if (u instanceof Customer) {
+		if (u.getType().equals("customer")) {
 			getCustomerView().start();
-		} else if (u instanceof Employee) {
+		} else if (u.getType().equals("employee")) {
 			getEmployeeView().start();
 		} else {
 			System.out.println(u.getClass());
